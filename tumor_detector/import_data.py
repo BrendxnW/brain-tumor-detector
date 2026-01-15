0059b712bd23caec29b9715275a6a812c3fb9e06
BrendxnW <BrendonHorse18@gmail.com>
2026-01-14 18:41:31 -0800

    fix import datafile to include required imports, and copied template from github

--- tumor_detector/import_data.py
+++ tumor_detector/import_data.py
@@ -1,31 +1,47 @@
+import os
+
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+import torch 
 import matplotlib.pyplot as plt
-from ucimlrepo import fetch_ucirepo
-from sklearn.preprocessing import StandardScaler
+from torch import Tensor
+from torch.utils.data import Dataset
 
 
-def transform_delta(X_train: pd.DataFrame, X_test: pd.DataFrame):
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-    return X_train_scaled, X_test_scaled
 
-def main():
-    dataset = fetch_ucirepo(id=696)
 
-    x = dataset.data.features.copy()
-    y = dataset.data.targets["Target"]
 
-    models = {
-        "Random Forest":{
-            "model": RandomForestClassifier(random_state=42),
-            "params":{
-                "n_estimators": [100, 200],
-                "max_depth": [None, 10, 20]
-            }
 
-        }
+class CustomImageDataset(Dataset):
+    def __init__(
+        self,
+        annotations_file,
+        img_dir,
+        transform = None,
+        target_transform = None,
+    ) -> None:
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
 
-    }
+    def __len__(self) -> int:
+        return len(self.img_labels)
+
+    def __getitem__(self, idx: int) -> tuple[Tensor, Any]:
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = decode_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+
+if __name__ == "__main__":
+    dataset = CustomImageDataset("annotations.csv", ".")
+    print(dataset[0])
+    print(dataset[1])
+
+
 
